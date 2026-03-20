8f9e27c937518537ad6f55713e437669a45cf1e8
Ubuntu <azure@Lokeshvirtualmachine.cvhytzxrwxke1cmspy1mqxcn2h.syx.internal.cloudapp.net>
2026-03-20 08:44:09 +0000

    updated config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   apps: [
     {
+<<<<<<< Updated upstream
       name: "backend",
       script: "venv/bin/python",
       args: "backend/manage.py runserver 0.0.0.0:8000",
@@ -11,6 +12,12 @@ module.exports = {
       script: "npm",
       args: "run dev",
       cwd: "/home/azure/stock-portfolio-project/frontend"
+=======
+      name: "stock-backend",
+      script: "venv/bin/python",
+      args: "backend/manage.py runserver 0.0.0.0:8001",
+      cwd: "/home/azure/stock-portfolio-project"
+>>>>>>> Stashed changes
     }
   ]
 };
