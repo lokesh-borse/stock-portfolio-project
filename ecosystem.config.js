5d59547e72ee29d666972fae70a8a2dd2d8ded52
Lokesh borse <149431066+lokesh-borse@users.noreply.github.com>
2026-03-20 14:36:24 +0530

    Update backend app name and port in config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,9 @@
 module.exports = {
   apps: [
-    {
-<<<<<<< Updated upstream
-      name: "backend",
-      script: "venv/bin/python",
-      args: "backend/manage.py runserver 0.0.0.0:8000",
-      cwd: "/home/azure/stock-portfolio-project"
-    },
-    {
-      name: "frontend",
-      script: "npm",
-      args: "run dev",
-      cwd: "/home/azure/stock-portfolio-project/frontend"
-=======
       name: "stock-backend",
       script: "venv/bin/python",
       args: "backend/manage.py runserver 0.0.0.0:8001",
       cwd: "/home/azure/stock-portfolio-project"
->>>>>>> Stashed changes
     }
   ]
 };
